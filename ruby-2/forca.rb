6f5a0538a687e549d66b66f4eef03ba069f67597
Diego Araújo <diesaraujo@gmail.com>
2016-01-29 15:58:29 -0200

    Added 'UI' and refactoring the code

--- ruby-2/forca.rb
+++ ruby-2/forca.rb
@@ -1,34 +1,4 @@
-def da_boas_vindas
-   puts "Bem-vindo ao jogo da forca"
-   puts "Qual é o seu nome?"
-   nome = gets.strip
-   puts "\n\n\n\n"
-   puts "Começaremos o jogo para você, #{nome}."
-   nome
-end
-
-def escolhe_palavra_secreta
-   puts "Escolhendo uma palavra secreta..."
-   palavra_secreta = "programador"
-   puts "Palavra secreta com #{palavra_secreta.size} letras...Boa sorte!!"
-   palavra_secreta
-end
-
-def nao_quer_jogar?
-   puts "Deseja Jogar novamente? (S/N)"
-   quero_jogar = gets.strip
-   nao_quero_jogar = quero_jogar.upcase == "N"
-end
-
-def pede_um_chute (erros, chutes)
-   puts "\n\n\n"
-   puts "Erros até agora: #{erros}"
-   puts "Chutes até agora: #{chutes}"
-   puts "Entre com uma letra ou uma palavra"
-   chute = gets.strip
-   puts "Será que acertou? Você chutou #{chute}"
-   chute
-end
+require_relative 'ui'
 
 def joga(nome)
    palavra_secreta = escolhe_palavra_secreta 
@@ -39,7 +9,7 @@ def joga(nome)
    while erros < 5
          chute = pede_um_chute erros, chutes
          if chutes.include? chute
-            puts "Você já chutou a letra - #{chute}"
+            avisa_chute_efetuado chute
             next
          end
          chutes << chute
@@ -49,33 +19,30 @@ def joga(nome)
             letra_procurada = chute[0]
             total_encontrado = palavra_secreta.count letra_procurada
             if total_encontrado == 0
-                  puts "Letra não econtrada"
+                  avisa_letra_nao_encontrada
                   erros += 1
                   else
-                     puts "Letra encontrada #{total_encontrado} vezes." 
+                     avisa_letra_encontrada total_encontrado
                   end
             else
                acertou = chute == palavra_secreta
                if acertou
-                     puts "Parabéns! Acertou!"
+                     avisa_acertou_palavra
                      pontos_ate_agora += 100
                      break
                else
-                     puts "Que pena, errou"
+                     avisa_errou_palavra
                      pontos_ate_agora -= 30
                      erros += 1
                end
          end
    end
-
-   puts "Você ganhou #{pontos_ate_agora} pontos."
+   avisa_pontos_ganhos pontos_ate_agora
 end
 
 nome = da_boas_vindas
 
 loop do
    joga nome
-   if nao_quero_jogar?
-      break
-   end
+   break if nao_quer_jogar?
 end
