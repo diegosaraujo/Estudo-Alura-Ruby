0f5bde59423b531783a34c8245323b7dcd00addc
diegosaraujo <diesaraujo@gmail.com>
2016-02-03 08:10:03 -0200

    Refactored the code, created new function 'calcula_nova_posicao'

--- ruby-3/fogefoge.rb
+++ ruby-3/fogefoge.rb
@@ -17,25 +17,30 @@ def encontra_jogador(mapa)
    #não achei
 end
 
+def calcula_nova_posicao(direcao, heroi)
+ case direcao
+           when "W"
+                  heroi[0] -=1
+            when "S"
+                  heroi[0] +=1
+            when "A"
+                  heroi[1] -= 1
+            when "D"
+                  heroi[1] += 1
+      end
+      heroi
+end
+
 def joga(nome)
   mapa = le_mapa 1
 
   while true
          desenha mapa
-        direcao = pede_movimento
-        heroi = encontra_jogador mapa
-        mapa[heroi[0]] [heroi[1]] = " "
-        case direcao
-              when "W"
-                     heroi[0] -=1
-               when "S"
-                     heroi[0] +=1
-               when "A"
-                     heroi[1] -= 1
-               when "D"
-                     heroi[1] += 1
-            end
-            mapa[heroi[0]] [heroi[1]] = "H"
+         direcao = pede_movimento
+         heroi = encontra_jogador mapa
+         mapa[heroi[0]] [heroi[1]] = " "
+         nova_posicao = calcula_nova_posicao direcao, heroi
+         mapa[nova_posicao[0]] [nova_posicao[1]] = "H"
    end
 end
 
