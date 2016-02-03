da4473c95a76cbf34466bc822627a6b9e22ad04b
diegosaraujo <diesaraujo@gmail.com>
2016-02-03 06:27:31 -0200

    Adding logic in my code

--- ruby-3/ui.rb
+++ ruby-3/ui.rb
@@ -6,3 +6,12 @@ def da_boas_vindas
     puts "Começaremos o Jogo para você, #{nome}"
     nome
 end
+
+def desenha(mapa)
+   puts mapa
+end
+
+def pede_movimento
+   puts "Para aonde deseja ir?"
+   movimento = gets.strip
+end
