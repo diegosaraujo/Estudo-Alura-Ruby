4162878f45e1a4b2c69241a0cf1c468b27ed035b
Diego Araújo <diesaraujo@gmail.com>
2016-02-04 11:36:29 -0200

    Corrected the fail on moviment of hero

--- ruby-3/fogefoge.rb
+++ ruby-3/fogefoge.rb
@@ -18,6 +18,7 @@ def encontra_jogador(mapa)
 end
 
 def calcula_nova_posicao(heroi, direcao)
+  heroi = heroi.dup
  case direcao
            when "W"
                   heroi[0] -=1
@@ -33,33 +34,37 @@ end
 
 def posicao_valida?(mapa, posicao)
   linhas = mapa.size
-  colunas = mapa[0].size
-  estourou_linhas = posicao[0] < 0 || posicao[0] >= linhas
-  estorou_colunas = posicao[1] < 0 || posicao[1] >= colunas
+    colunas = mapa[0].size
 
-    if estourou_linhas || estorou_colunas
-      return false
+    estourou_linha = posicao[0] < 0 || posicao[0] >= linhas
+    estourou_coluna = posicao[1] < 0 || posicao[1] >= colunas
+
+    if estourou_linha || estourou_coluna
+        return false
     end
+
     if mapa[posicao[0]][posicao[1]] == "X"
-      return false
+        return false
     end
+
     true
 end
 
 def joga(nome)
-  mapa = le_mapa 1
+  mapa = le_mapa(1)
+      while true
+          desenha mapa
+          direcao = pede_movimento
 
-  while true
-         desenha mapa
-         direcao = pede_movimento
-         heroi = encontra_jogador mapa
-         nova_posicao = calcula_nova_posicao heroi, direcao
-         if !posicao_valida? mapa, nova_posicao
-            next
+          heroi = encontra_jogador mapa
+          nova_posicao = calcula_nova_posicao heroi, direcao
+          if !posicao_valida? mapa, nova_posicao
+              next
           end
-         mapa[heroi[0]][heroi[1]] = " "
-         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-   end
+
+          mapa[heroi[0]][heroi[1]] = " "
+          mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+      end
 end
 
 def inicia_fogefoge
